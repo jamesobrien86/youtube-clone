cf50d4f293a40b3cd49fd394b00cc483a3f0233d
jamesobrien86 <jamesobrien.web@gmail.com>
2019-06-27 17:14:50 +0300

    wip - added search bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component{
     render(){
         return(
-            <h1>Hi</h1>
+            <div className="ui container">
+                <SearchBar />
+            </div>            
         )
     }
 }
